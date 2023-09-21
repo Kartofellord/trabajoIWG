a032c04f7420732432b3dbb528ede41704e873b8
Kartofellord <oscar.felipepf@gmail.com>
2023-09-21 00:07:53 -0300

    Test Base de datos (no cambie nada)

--- trabajoIWG/trabajoIWG/settings.py
+++ trabajoIWG/trabajoIWG/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'trabajoIWG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_pyscopg2',
+        'NAME': 'GreenMap',
+        'USER' : 'postgres',
+        'PASSWORD' : 'grupo8',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
