fcf85153d7ce18a233ea1ccb0e1b71d47e82d9a9
Kartofellord <oscar.felipepf@gmail.com>
2023-09-24 00:33:09 -0300

    Prime version base de datos

--- trabajoIWG/user/models.py
+++ trabajoIWG/user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class userData(models.Model):
+    userClass = models.PositiveIntegerField() #Distincion entre persona y entidad
+    userType = models.CharField() #Rubro o rol de la persona
+    userName = models.CharField(max_length=15, unique=True) #Nombre de usuario
+    realName = models.CharField() #Nombre real de la persona (opcional)
+    userEmail = models.CharField(unique=True, primary_key=True) #Email del usuario
+    userCountry = models.CharField() #Pais del usuario
+    userFollwers = models.PositiveIntegerField() #Cantidad de segiudores
+    userQualy = models.FloatField() #Calificacion que tendra el usuario (0 a 5)
+    userDescr= models.CharField(max_length=150) #Descripcion del usuario *
+    #puede quiza incluir foto
