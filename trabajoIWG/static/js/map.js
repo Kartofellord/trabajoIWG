27cbc9727666ad2fdf089617d0db6215512f96c8
Kartofellord <oscar.felipepf@gmail.com>
2023-10-18 21:39:11 -0300

    Modifique el home y la barra de arriba

---
+++ trabajoIWG/static/js/map.js
@@ -0,0 +1,13 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([2.333333, 48.866667]),
+      zoom: 10
+
+    })
+});
